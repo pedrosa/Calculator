61c72f85ed8938f033e1dfb90b8c1317ce2d56c8
Pedro <pedro.sa@stone.com.br>
2023-07-09 13:45:05 -0300

    implement logic

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,3 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿class Calculadora
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine(
+                "Escolha uma das opções 1 (Somar), 2 (Subtrair), 3 (Multiplicar), 4 (Dividir) e 0 (Sair):");
+            int input = int.Parse(Console.ReadLine());
 
-Console.WriteLine("Hello, World!");
+            if (input == 0)
+            {
+                return;
+            }
+
+            Console.Write("Digite o primeiro número:");
+            double a = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite o segundo número:");
+            double b = Double.Parse(Console.ReadLine());
+
+            double result;
+
+            switch (input)
+            {
+                case 1:
+                    result = a + b;
+                    Console.WriteLine($"O resultado da soma é: {result}");
+                    break;
+                case 2:
+                    result = a - b;
+                    Console.WriteLine($"O resultado da subtração é: {result}");
+                    break;
+                case 3:
+                    result = a * b;
+                    Console.WriteLine($"O resultado da multiplicação é: {result}");
+                    break;
+                case 4:
+                    if (b != 0)
+                    {
+                        result = a / b;
+                        Console.WriteLine($"O resultado da divisão é: {result}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Erro: Divisão por zero.");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida.");
+                    break;
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
